4be2244b839f8f901c7c32887af60f4d3f65563b
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-21 15:53:31 -0300

    feat: save first draft

--- desenvolvimento-dinamico-JSON/manipulando_JSON.js
+++ desenvolvimento-dinamico-JSON/manipulando_JSON.js
@@ -2,4 +2,24 @@ const fetch = require('node-fetch');
 
 fetch('https://fakestoreapi.com/products')
             .then(response=>response.json())
-            .then(json=>console.log(json))
+            .then(data => {
+              data.forEach(product => {
+                addProductToPage(product);
+              });
+            })
+
+function addProductToPage(product) {
+  const productDiv = document.createElement('div');
+  productDiv.classList.add('product');
+  productDiv.setAttribute('product-id', product.id).add('product');
+  productDiv.innerHTML = `
+  <li>  
+    <h2>${product.title}</h2>
+    <img src="${product.image}" alt="${product.title}">
+    <p>${product.description}</p>
+    <p>Price: $${product.price}</p>
+    <product
+  </li>
+  `;
+  document.body.appendChild(productDiv);
+}
