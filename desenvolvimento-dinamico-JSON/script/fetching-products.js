df4324b2c82f75d1e8fc9f9fbc245cfabe733de1
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-21 17:25:42 -0300

    feat: add dynamic list of products in site
    -still failing to add buttons logic correctly
    -code need more adaptation from last shopping cart

--- desenvolvimento-dinamico-JSON/script/fetching-products.js
+++ desenvolvimento-dinamico-JSON/script/fetching-products.js
@@ -1,4 +1,5 @@
-const fetch = require("node-fetch");
+import Button from "./class-button.js";
+// const fetch = require("node-fetch");
 
 fetch("https://fakestoreapi.com/products")
   .then((response) => response.json())
@@ -6,9 +7,11 @@ fetch("https://fakestoreapi.com/products")
     data.forEach((product) => {
       addProductToPage(product);
     });
-  });
+  })
+  .catch((error) => console.error("Error fetching products:", error));
 
 function addProductToPage(product) {
+  let productList = document.querySelector("#products-list");
   const productDiv = document.createElement("div");
   productDiv.classList.add("product");
   productDiv.setAttribute("product-id", product.id);
@@ -28,7 +31,8 @@ function addProductToPage(product) {
             <span class="text-grey">Rating:</span
             ><span class="font-weight-bold">&nbsp;${product.rating.rate} (${product.rating.count} votes)</span>
           </div>
-            <div class="size mr-1">
+          <br>
+          <div class="size mr-1">
             <span class="text-grey">Description:</span
             ><span class="font-weight-bold">${product.description}</span>
           </div>
@@ -47,5 +51,24 @@ function addProductToPage(product) {
       </div>
     </div>
   `;
-  document.body.appendChild(productDiv);
+  productList.appendChild(productDiv);
+
+  const plusButtons = document.querySelectorAll(".fa-plus");
+  const minusButtons = document.querySelectorAll(".fa-minus");
+  const trashButtons = document.querySelectorAll(".fa-trash");
+  //starting buttons
+  plusButtons.forEach(function (plusButton) {
+    const oneButton = new Button(plusButton);
+    oneButton.element.addEventListener("click", oneButton.addItem);
+  });
+
+  minusButtons.forEach(function (minusButton) {
+    const oneButton = new Button(minusButton);
+    oneButton.element.addEventListener("click", oneButton.removeItem);
+  });
+
+  trashButtons.forEach(function (trashButton) {
+    const oneButton = new Button(trashButton);
+    oneButton.element.addEventListener("click", oneButton.deleteProduct);
+  });
 }
