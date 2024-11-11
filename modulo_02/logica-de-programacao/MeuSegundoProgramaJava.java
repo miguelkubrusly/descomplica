d0ed2a56813993797cd659445d89534dbf8a9eb6
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-11-11 18:03:29 -0300

    feat(LogProg): ✨ make java 2nd simple prog

---
+++ modulo_02/logica-de-programacao/MeuSegundoProgramaJava.java
@@ -0,0 +1,11 @@
+class MeuSegundoProgramaJava{
+  public static void main(String[] arg) {
+    int idade = 31;
+    String curso = "Engenharia de Software";
+    double peso = 80;
+    char genero='M';
+    String nome="Miguel Kubrusly";
+    System.out.println("Eu me chamo "+nome+", tenho "+idade+" anos, sou do gênero: "+genero+", peso "+peso+" kg e estudo "+curso+".");
+    System.exit(0);
+  };
+}
