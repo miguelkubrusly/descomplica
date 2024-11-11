27645449d36634d6dfdae0f81c7056c8094fd4a8
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-11-11 17:45:28 -0300

    feat(LogProg): 💥 first java prog

---
+++ modulo_02/logica-de-programacao/MeuPrimeiroProgramaJava.java
@@ -0,0 +1,13 @@
+class MeuPrimeiroProgramaJava{
+  public static void main(String entrada[]) {
+    int inteiro = 31;
+    char caractere = 'C';//deve estar em aspas simples
+    double real = 1.71;
+    String frase="Miguel Kubrusly ";
+    boolean VF=true;
+    if (VF ==true){
+      System.out.println("Eu sou a "+frase+". Tenho "+inteiro+" anos, "+real+"m de altura e meu sobrenome do meio começa com "+caractere+".");
+    }
+    System.exit(0);
+  }
+}
